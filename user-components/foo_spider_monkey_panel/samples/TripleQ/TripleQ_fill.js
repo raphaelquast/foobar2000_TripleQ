fcca4ced7684f66660b7b6865ff8a81f061f2221
Raphael <raphael.quast@geo.tuwien.ac.at>
2020-07-20 14:02:23 +0200

    move all relevant files to themes/TripleQ - the component folder gets deleted on component-update!

--- user-components/foo_spider_monkey_panel/samples/TripleQ/TripleQ_fill.js
+++
@@ -1,8 +0,0 @@
-include(fb.ComponentPath + 'docs\\Helpers.js');
-window.Repaint()
-
-function on_size(width, height) { window.Repaint() }
-
-function on_paint(gr) {
-	gr.FillSolidRect(0, 0, window.Width, window.Height, RGB(37, 37, 37));
-}
